84e1c9f170269fcae8d1d8edf46c09658eb0c075
Shirasa P <shirasapraja@gmail.com>
2023-09-25 16:07:07 -0600

    remove mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 let mongoose = require('mongoose');
+console.log(process.env.MONGO_URI);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
