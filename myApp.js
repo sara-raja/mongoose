fa0d345b2b6372be8ce52a04d44c6fb8676f9f7f
Shirasa P <shirasapraja@gmail.com>
2023-09-25 16:34:24 -0600

    add mongoose schemas

--- myApp.js
+++ myApp.js
@@ -4,54 +4,200 @@ console.log(process.env.MONGO_URI);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+});
 
+let Person = mongoose.model('Person', personSchema)
+
+let person = new Person({
+  name: "Ada Lovelace",
+  age: 44,
+  favouriteFoods: ['bbq', 'rice', 'grapes']
+});
+
+// create and save model:
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  person
+    .save()
+    .then((doc) => {
+      console.log(doc)
+      done(null, doc);
+
+    })
+    .catch((err) => {
+      console.error(err)
+    });
 };
+
+// adding arry of people:
+let arrayOfPeople = [{
+  name: "Franz Schubert",
+  age: 65,
+  favouriteFoods: ['wine', 'ratatouie', 'grapes']
+}, {
+  name: "Beethoven",
+  age: 32,
+  favouriteFoods: ['bread', 'cheese', 'venison']
+}, {
+  name: "Beyonce",
+  age: 47,
+  favouriteFoods: ['bbq', 'seafood boil', 'corn bread']
+}]
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople)
+    .then((doc) => {
+      console.log(doc)
+      done(null, doc);
+
+    })
+    .catch((err) => {
+      console.error(err)
+    });
 };
 
+// finding and searching the database:
+// let personName = {name: 'Beyonce'}
+
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName })
+    .then((doc) => {
+      console.log(doc)
+      done(null, doc);
+
+    })
+    .catch((err) => {
+      console.error(err)
+    });
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food })
+    .then((doc) => {
+      console.log(doc)
+      done(null, doc)
+
+    })
+    .catch((err) => {
+      console.error(err)
+    })
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({ _id: personId })
+    .then((doc) => {
+      console.log(doc)
+      done(null, doc)
+
+    })
+    .catch((err) => {
+      console.error(err)
+    })
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  Person.findById({ _id: personId })
+    .then((doc) => {
+      console.log(doc)
+      doc.favoriteFoods.push(foodToAdd)
+      doc.save()
+        .then((docTwo) => {
+          console.log(docTwo)
+          done(null, docTwo);
+
+        })
+        .catch((err) => {
+          console.error(err)
+        });
+
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+
 };
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    {
+      name: personName
+    },
+    {
+      age: ageToSet
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  )
+    .then((doc) => {
+      console.log(doc)
+      done(null, doc);
+
+    })
+    .catch((err) => {
+      console.error(err)
+    });
+
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findOneAndRemove(
+    {
+      _id: personId
+    }
+  )
+    .then((doc) => {
+      console.log(doc)
+      done(null, doc);
+
+    })
+    .catch((err) => {
+      console.error(err)
+    });
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
+  Person.remove({ name: nameToRemove })
+    .then((doc) => {
+      console.log(doc)
+      done(null, doc);
 
-  done(null /*, data*/);
+    })
+    .catch((err) => {
+      console.error(err)
+    });
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select({
+      name: true, favoriteFoods: truef
+        .then((doc) => {
+          console.log(doc)
+          done(null, doc);
+
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+      // .exec()
+    })
 };
 
 /** **Well Done !!**
